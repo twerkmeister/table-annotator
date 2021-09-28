bb8161cd090661955e117c100936aa380e2a5af8
Thomas Werkmeister <thomas@werkmeister.me>
2021-09-28 14:50:57 +0200

    retrieving specific images

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 from typing import Text
 import os
-from flask import Flask
+from flask import Flask, send_from_directory
 
 IMAGE_PATH = "image_path"
 
@@ -10,12 +10,16 @@ def create_app(image_path: Text):
     app.config[IMAGE_PATH] = image_path
 
     @app.route('/images')
-    def get_images():
+    def list_images():
         files = os.listdir(app.config[IMAGE_PATH])
         allowed_extensions = {".jpeg", ".jpg"}
         relevant_files = [f for f in files
                           if os.path.splitext(f)[1] in allowed_extensions]
         return {"images": relevant_files}
+
+    @app.route('/image/<file_name>')
+    def get_image(file_name):
+        return send_from_directory(app.config[IMAGE_PATH], file_name)
 
     return app
 
--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,7 +1,13 @@
 
 
-def test_images(test_client):
+def test_list_images(test_client):
     response = test_client.get("/images")
     images = response.json["images"]
     assert len(images) == 3
     assert "ducks_1.jpeg" in images
+
+
+def test_get_image(test_client):
+    response = test_client.get("/image/ducks_1.jpeg")
+    assert response.status_code == 200
+    assert len(response.data) > 10000
