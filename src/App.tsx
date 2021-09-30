4ca6cddbbda2225d20fd81a012e001b2cff75f31
Thomas Werkmeister <thomas@werkmeister.me>
2021-09-30 16:53:51 +0200

    centered document, interactive table outline painting

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,38 @@ const keyMap = {
     PREVIOUS_IMAGE: "a",
     NEXT_IMAGE: "d",
     INCREASE_ROTATION: "w",
-    DECREASE_ROTATION: "s"
+    DECREASE_ROTATION: "s",
+    ESC: "esc"
 };
 
 type Point = {
     x: number,
     y: number
+}
+
+function subtractPoints(p: Point, p2: Point): Point {
+    return {x: p.x - p2.x, y: p.y - p2.y}
+}
+
+function addPoints(p: Point, p2: Point): Point {
+    return {x: p.x + p2.x, y: p.y + p2.y}
+}
+
+function rotatePoint(p: Point, angle: number, rotationCenter: Point = {x: 0, y: 0}): Point {
+    const sinAngle = Math.sin(angle);
+    const cosAngle = Math.cos(angle);
+
+    const translated = subtractPoints(p, rotationCenter)
+    const rotated = {
+        x: translated.x * cosAngle - translated.y * sinAngle,
+        y: translated.x * sinAngle + translated.y * cosAngle
+    }
+    return addPoints(rotated, rotationCenter)
+}
+
+type Rectangle = {
+    topLeft: Point,
+    bottomRight: Point
 }
 
 type Image = {
@@ -23,21 +49,34 @@ type Image = {
 
 type UnfinishedTable = {
     firstPoint: Point
+    rotationDegrees: number,
 }
 
-function makeTable(p1: Point, p2: Point, rotationDegrees: number): Table {
+function getPageOffset(el: Element): Point {
+    const rect = el.getBoundingClientRect()
+    const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+
+    return { y: rect.top + scrollTop, x: rect.left + scrollLeft }
+}
+
+function calcRectangle(p1: Point, p2: Point): Rectangle {
     const [topLeftX, bottomRightX] = p1.x <= p2.x ? [p1.x, p2.x] : [p2.x, p1.x]
     const [topLeftY, bottomRightY] = p1.y <= p2.y ? [p1.y, p2.y] : [p2.y, p1.y]
     const topLeft = {x: topLeftX, y: topLeftY}
     const bottomRight = {x: bottomRightX, y: bottomRightY}
+    return {topLeft, bottomRight}
+}
+
+function makeTable(p1: Point, p2: Point, rotationDegrees: number): Table {
+    const outline = calcRectangle(p1, p2)
     return {
-        topLeft, bottomRight, rotationDegrees
+        outline, rotationDegrees
     }
 }
 
 type Table = {
-    topLeft: Point,
-    bottomRight: Point,
+    outline: Rectangle,
     rotationDegrees: number,
 }
 
@@ -46,18 +85,25 @@ type AnnotatorState = {
     images?: Image[],
     currentImageIndex: number,
     unfinishedTable?: UnfinishedTable,
+    mousePosition: Point,
+    documentPosition?: Point,
     rotationDegrees: number,
     tables: Table[],
     fetchImages: () => void
     setImageIndex: (idx: number) => void
     outlineTable: (p: Point, rotationDegrees: number) => void,
     rotate: (degrees: number) => void,
+    setMousePosition: (mousePosition: Point) => void,
+    setDocumentPosition: (documentPosition: Point) => void,
+    removeUnfinishedTable: () => void,
 }
 
 const useStore = create<AnnotatorState>((set, get) => ({
     images: undefined,
     currentImageIndex: 0,
     unfinishedTable: undefined,
+    mousePosition: {x: 0, y: 0},
+    documentPosition: undefined,
     rotationDegrees: 0,
     tables: [],
     fetchImages: async() => {
@@ -71,7 +117,7 @@ const useStore = create<AnnotatorState>((set, get) => ({
     setImageIndex: (idx: number) => {
         const images = get().images
         if(images && idx >= 0 && idx < images.length){
-            set({ currentImageIndex: idx })
+            set({ currentImageIndex: idx, rotationDegrees: 0, documentPosition: undefined})
         }
     },
     outlineTable: (p: Point, rotationDegrees: number) => {
@@ -81,12 +127,21 @@ const useStore = create<AnnotatorState>((set, get) => ({
             const newTable = makeTable(unfinishedTable.firstPoint, p, rotationDegrees)
             set({unfinishedTable: undefined, tables: [...currentTables, newTable]})
         } else {
-            set({unfinishedTable: {firstPoint: p}})
+            set({unfinishedTable: {firstPoint: p, rotationDegrees}})
         }
     },
     rotate: (degrees: number) => {
         const rotationDegrees = get().rotationDegrees + degrees
         set({rotationDegrees})
+    },
+    setMousePosition: (mousePosition: Point) => {
+        set({mousePosition})
+    },
+    setDocumentPosition: (documentPosition: Point) => {
+        set({documentPosition})
+    },
+    removeUnfinishedTable: () => {
+        set ({unfinishedTable: undefined})
     }
 }))
 
@@ -95,30 +150,45 @@ function App() {
     const setImageIndex = useStore(state => state.setImageIndex)
     const rotate = useStore(state => state.rotate)
     const tables = useStore(state => state.tables)
+    const unfinishedTable = useStore(state => state.unfinishedTable)
     const imageIdx = useStore(state => state.currentImageIndex)
     const images = useStore(state => state.images)
+    const setMousePosition = useStore(state => state.setMousePosition)
+    const removeUnfinishedTable = useStore(state => state.removeUnfinishedTable)
     useEffect(() => fetchImages())
+
+    const handleMouseMove = (e: React.MouseEvent<Element, MouseEvent>) => {
+        setMousePosition({x: e.pageX, y: e.pageY})
+    }
 
     const hotkeyHandlers = {
         PREVIOUS_IMAGE:  () => setImageIndex(imageIdx - 1),
         NEXT_IMAGE:  () => setImageIndex(imageIdx + 1),
         INCREASE_ROTATION: () => rotate(0.5),
-        DECREASE_ROTATION: () => rotate(-0.5)
-    };
+        DECREASE_ROTATION: () => rotate(-0.5),
+        ESC: () => {}
+    }
+
+    if (unfinishedTable) {
+        hotkeyHandlers.ESC = removeUnfinishedTable
+    }
 
     if(typeof(images) != "undefined" && images.length > 0) {
         const image = images[imageIdx]
         const imageCenter: Point = {x: image.width / 2, y: image.height / 2}
         return (
-            <div className="App">
+            <div className="App" onMouseMove={e => handleMouseMove(e)}>
                 <HotKeys keyMap={keyMap} handlers={hotkeyHandlers} allowChanges={true}>
-                    <DocumentImage {...image}/>
+                    <DocumentImage {...image} />
                     {tables.map((t, i) => {
                         return (
-                            <TableElement key={i} tableTopLeft={t.topLeft} tableBottomRight={t.bottomRight}
+                            <TableElement key={i} tableTopLeft={t.outline.topLeft}
+                                          tableBottomRight={t.outline.bottomRight}
                                           imageCenter={imageCenter} tableRotation={t.rotationDegrees}/>
                         )
                     })}
+                    {typeof(unfinishedTable) != "undefined" ? <StartedTable {...unfinishedTable}
+                                                                            imageCenter={imageCenter} /> : null}
                 </HotKeys>
             </div>
         );
@@ -133,26 +203,70 @@ function App() {
     }
 }
 
+function StartedTable(props: { firstPoint: Point, rotationDegrees: number, imageCenter: Point } ) {
+    const rotationDegrees = useStore(state => state.rotationDegrees)
+    const mousePosition = useStore(state => state.mousePosition)
+    const documentPosition = useStore(state => state.documentPosition)
+    const outlineTable = useStore(state => state.outlineTable)
+
+    if(documentPosition) {
+        const handleCanvasClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+            e.preventDefault()
+            if (documentPosition) {
+                outlineTable({x: e.pageX - documentPosition.x, y: e.pageY - documentPosition.y}, rotationDegrees)
+            }
+        }
+        const mouseOffsetPosition = {x: mousePosition.x - documentPosition.x, y: mousePosition.y - documentPosition.y}
+        const rectangle = calcRectangle(props.firstPoint, mouseOffsetPosition)
+        return (
+            <div className="table"
+                 style={{
+                     transform: `rotate(${rotationDegrees - props.rotationDegrees}deg) translate(${rectangle.topLeft.x}px, ${rectangle.topLeft.y}px)`,
+                     width: `${rectangle.bottomRight.x - rectangle.topLeft.x}px`,
+                     height: `${rectangle.bottomRight.y - rectangle.topLeft.y}px`,
+                     transformOrigin: `${props.imageCenter.x}px ${props.imageCenter.y}px`
+                 }}
+                 onClick={e => handleCanvasClick(e)}/>
+        )
+    } else {
+        return (<div/>)
+    }
+}
+
 function TableElement(props: {tableTopLeft: Point, tableBottomRight: Point, tableRotation: number, imageCenter: Point}) {
     const rotationDegrees = useStore(state => state.rotationDegrees)
     return (
-        <div className="table" style={{transform: `rotate(${rotationDegrees - props.tableRotation}deg) translate(${props.tableTopLeft.x}px, ${props.tableTopLeft.y}px)`,
-        width: `${props.tableBottomRight.x - props.tableTopLeft.x}px`, height: `${props.tableBottomRight.y - props.tableTopLeft.y}px`,
-        transformOrigin: `${props.imageCenter.x}px ${props.imageCenter.y}px`}}/>
+        <div className="table"
+             style={{transform: `rotate(${rotationDegrees - props.tableRotation}deg) translate(${props.tableTopLeft.x}px, ${props.tableTopLeft.y}px)`,
+                     width: `${props.tableBottomRight.x - props.tableTopLeft.x}px`,
+                     height: `${props.tableBottomRight.y - props.tableTopLeft.y}px`,
+                     transformOrigin: `${props.imageCenter.x}px ${props.imageCenter.y}px`}}/>
     )
 }
 
 function DocumentImage(image: Image) {
+    const ref = React.useRef<HTMLImageElement>(null)
     const outlineTable = useStore(state => state.outlineTable)
     const rotationDegrees = useStore(state => state.rotationDegrees)
+    const setDocumentPosition = useStore(state => state.setDocumentPosition)
+    const documentPosition = useStore(state => state.documentPosition)
 
+    useEffect(() => {
+        const imageRef = ref.current
+        if (imageRef && typeof(documentPosition) == "undefined") {
+            const newDocumentPosition = getPageOffset(imageRef)
+            setDocumentPosition(newDocumentPosition)
+        }
+    })
     const handleCanvasClick = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
         e.preventDefault()
-        outlineTable({x: e.pageX, y: e.pageY}, rotationDegrees)
+        if (documentPosition) {
+            outlineTable({x: e.pageX - documentPosition.x, y: e.pageY - documentPosition.y}, rotationDegrees)
+        }
     }
 
     return (
-        <img className="documentImage" src={image.src} width={image.width} height={image.height}
+        <img ref={ref} className="documentImage" src={image.src} width={image.width} height={image.height}
         style={{transform: `rotate(${rotationDegrees}deg)`}} alt="The document"
              onClick={e => handleCanvasClick(e)}/>
     )
