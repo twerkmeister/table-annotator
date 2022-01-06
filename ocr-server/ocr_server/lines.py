6f083dc5a11871d2bf750d387a6bec783a6aa6a3
Thomas Werkmeister <thomas@werkmeister.me>
2022-01-06 10:50:45 +0100

    splitting lines in ocr server

---
+++ ocr-server/ocr_server/lines.py
@@ -0,0 +1,31 @@
+from typing import List
+
+import numpy as np
+import cv2
+import scipy.signal
+
+
+def find_lines(image: np.ndarray) -> List[np.ndarray]:
+    """Split a cell image into multiple text lines."""
+    window_size = 30
+    # image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_blurred = cv2.medianBlur(image, 5)
+    image_inverted = cv2.bitwise_not(image_blurred)
+    image_horizontal_squared_diffs = \
+        np.sum(np.square(np.diff(image_inverted, axis=1)), axis=1)
+    gaussian_window = scipy.signal.windows.gaussian(window_size, 5)
+    values = np.convolve(image_horizontal_squared_diffs, gaussian_window, 'same')
+    value_diffs = np.diff(values)
+    diff_signs = np.sign(value_diffs)
+    sign_diffs = np.diff(diff_signs)
+    local_maxima = [i for i, sign_diff in enumerate(sign_diffs) if sign_diff == -2]
+    local_maxima = [local_maximum for local_maximum in local_maxima
+                    if local_maximum > 8 and local_maximum + 8 < image.shape[0]]
+
+    lines = []
+    for local_maximum in local_maxima:
+        start = max(local_maximum - 15, 0)
+        end = min(local_maximum + 15, image.shape[0])
+        lines.append(image[start:end])
+
+    return lines
--- ocr-server/server.py
+++ ocr-server/server.py
@@ -5,6 +5,8 @@ from flask_cors import CORS
 import numpy as np
 from calamari_ocr.ocr.predict.predictor import Predictor, PredictorParams
 
+from ocr_server.lines import find_lines
+
 
 def create_app(script_info: Optional[ScriptInfo] = None):
     app = Flask(__name__)
@@ -16,12 +18,22 @@ def create_app(script_info: Optional[ScriptInfo] = None):
 
     @app.route('/ocr', methods=["POST"])
     def ocr():
-        images = [np.array(img) for img in request.json["images"]]
+        images = [np.array(img, dtype="uint8") for img in request.json["images"]]
 
+        images_text_lines = [find_lines(image) for image in images]
+        num_text_lines = [len(text_lines) for text_lines in images_text_lines]
+        flattened_line_images = [line for image_lines in images_text_lines
+                                for line in image_lines]
         predictions = [sample.outputs.sentence
-                       for sample in ocr_model.predict_raw(images)]
+                       for sample in ocr_model.predict_raw(flattened_line_images)]
 
-        return {"predictions": predictions}
+        merged_predictions = []
+        for i in range(len(num_text_lines)):
+            offset = sum(num_text_lines[:i])
+            lines_merged = "\n".join(predictions[offset:offset+num_text_lines[i]])
+            merged_predictions.append(lines_merged)
+
+        return {"predictions": merged_predictions}
 
     app.logger.info(f'Starting ocr server')
     return app
