fd9f648238763f29cd66a3291adf733236cd67bb
Thomas Werkmeister <thomas@werkmeister.me>
2023-02-14 16:15:17 +0100

    Only offering pre annotation if the image has preannotated data

--- src/components/MenuComponents/ApplyPreAnnotatedDataButton.tsx
+++ src/components/MenuComponents/ApplyPreAnnotatedDataButton.tsx
@@ -6,13 +6,18 @@ import {useStore} from "../../store";
 import {doesTableHaveTypes} from "../../util";
 
 const ApplyPreAnnotatedDataButton = () => {
+    const images = useStore(state => state.images)
+    const currentImageIndex = useStore(state => state.currentImageIndex)
     const tables = useStore(state => state.tables)
     const selectedTable = useStore(state => state.selectedTable)
     const ocrView = useStore(state => state.ocrView)
     const applyPreAnnotatedData = useStore(state => state.applyPreAnnotatedData)
 
-    const disabled = selectedTable === undefined || tables[selectedTable] === undefined
+    const disabled = images === undefined || images[currentImageIndex] === undefined ||
+        !images[currentImageIndex].hasPreAnnotatedData ||
+        selectedTable === undefined || tables[selectedTable] === undefined
         || !ocrView || !doesTableHaveTypes(tables[selectedTable])
+
     const handleClick = () => {
         if (disabled) return
         applyPreAnnotatedData()
